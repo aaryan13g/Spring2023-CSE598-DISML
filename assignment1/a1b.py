c1e7fe4a030393c50964090ca40fa31ef3d8c5c8
Aaryan Gupta <gupta.aaryan8@gmail.com>
2023-01-20 23:15:18 -0700

    Add code and logs

--- assignment1/a1b.py
+++ assignment1/a1b.py
@@ -1,1 +1,48 @@
-# a1b.py
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import time
+import datetime
+
+
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+def build_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Reshape(target_shape=(28, 28, 1), input_shape=(28, 28)))
+    model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(units=64, activation='relu'))
+    model.add(keras.layers.Dense(units=10, activation='softmax'))
+    return model
+
+
+start_time = time.time()
+
+model = build_model()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Create TensorBoard logs
+logdir = "logs/scalars/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
+# Train the model
+model.fit(x_train, y_train, epochs=5, callbacks=[tensorboard_callback])
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Test accuracy: ", test_acc)
+
+end_time = time.time()
+
+print('Total Time {} ms.'.format(end_time - start_time))
